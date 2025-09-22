281ab5fe4828b69f20972c6ed50143a01c86b4f6
dagas_sa <DagassaCoop@gmail.com>
2025-09-21 21:34:50 -0400

    feat: cv update

--- src/ui/components/LoadFileButton.tsx
+++ src/ui/components/LoadFileButton.tsx
@@ -4,7 +4,8 @@ import { FC } from "react";
 import Button, { IButtonProps } from "./Button";
 
 // Mock
-import cv from "@/assets/cv/Oleksandra_Babich_CV_Video_Producer.pdf";
+// import cv from "@/assets/cv/Oleksandra_Babich_CV_Video_Producer.pdf";
+import cv from '@/assets/cv/Video_Producer.pdf'
 
 const LoadFileButton: FC<IButtonProps> = ({
   children,
